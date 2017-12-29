f577c5d1aa7825386c4e84fe2da82a5a8b321daf
Juan Rivera Vargas <jrivera.v@outlook.com>
2017-12-29 14:47:12 -0300

    api get alumnos de un cursos

---
+++ controllers/CursoController.ts
@@ -0,0 +1,19 @@
+import {
+    getConnection
+} from "typeorm";
+import * as Entities from "../models/MySql/Index";
+
+export async function getAllAlumFromCurso(req, res, next) {
+    let connection = getConnection();
+    let idCurso = req.params.id;
+    let alumnos = await connection.getRepository(Entities.Curso).createQueryBuilder("c").
+    innerJoin("c.Alumnos","a").where(`c.Id = ${idCurso}`)
+    .select(["a.Nombre","a.Apellido"]).getRawMany();
+    if (alumnos.length == 0) {
+        res.status(404).json({
+            "error": "no existen alumnos"
+        });
+    } else {
+            res.status(200).json(alumnos);
+    }
+}
--- routers/routes.ts
+++ routers/routes.ts
@@ -2,6 +2,7 @@ import * as express from "express"; // el routing neceista del servidor express
 import * as auth from "../middlewares/authorization";
 import * as alumnosCtl from "../controllers/AlumnoController";
 import * as usuariosCtl from "../controllers/UsuarioController";
+import * as cursosCtl from "../controllers/CursoController";
 // exportamos la funcion que utiliza posteriormeente el archivo init.ts para iniciar el encapsulamiento de
 // retun controller --> retorna route( app )
 export default (app) => {
@@ -9,16 +10,19 @@ export default (app) => {
     const apiRoutes = express.Router(); // la utilizaremos para que englobe todas las rutas
     const alumnosRoutes = express.Router();
     const usuarioRoutes = express.Router();
+    const cursosRoutes = express.Router();
 
     //trae los alumnos que posee el propietario del token
     alumnosRoutes.get("/",auth.authorize("User"),alumnosCtl.getAllAlumFromUsr);
-
-    // esta linea crea un middleware el cual crea la ruta /alumnos y embebe los verbos y rutas que asignamos a alumnosRoute
     apiRoutes.use("/alumnos", alumnosRoutes);
    
     // routes para api usuarios
     usuarioRoutes.post("/authenticate", usuariosCtl.authenticateUser);
     apiRoutes.use("/usuarios",usuarioRoutes);
+
+     // routes para api cursos
+     cursosRoutes.get("/:id", cursosCtl.getAllAlumFromCurso);
+     apiRoutes.use("/cursos",cursosRoutes);
 
 
     app.use("/api", apiRoutes);
